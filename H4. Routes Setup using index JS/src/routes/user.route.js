00677916a76995e8fb7f77fa178bc5b54bac7582
virat1816 <viratpatel5880@gmail.com>
2023-08-10 11:37:06 +0530

    Routes Setup

---
+++ H4. Routes Setup using index JS/src/routes/user.route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+/** create user */
+router.post(
+    "/create-user",
+);
+
+/** Get user list */
+router.get(
+    "/list",
+);
+
+module.exports = router;
