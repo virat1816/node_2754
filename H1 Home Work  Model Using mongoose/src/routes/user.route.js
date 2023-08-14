ca7850902e124354b0f83ea3ea1cff4f57ded468
virat1816 <viratpatel5880@gmail.com>
2023-08-14 11:58:47 +0530

    PRACTICE

--- H1 Home Work  Model Using mongoose/src/routes/user.route.js
+++ H1 Home Work  Model Using mongoose/src/routes/user.route.js
@@ -1,51 +1,54 @@
-
 const express = require("express");
-const { userValidation } = require("../../validations");
-const { userController } = require("../../controllers");
+const {
+    userValidation
+} = require("../../validations");
+const {
+    userController
+} = require("../../controllers");
 const validate = require("../../middlewares/validate");
 
 const router = express.Router();
 
 /** create user */
 router.post(
-  "/create-user",
-  validate(userValidation.createUser),
-  userController.createUser
+    "/create-user",
+    validate(userValidation.createUser),
+    userController.createUser
 );
 
 /** Get user list */
 router.get(
-  "/list",
-  validate(userValidation.getUserList),
-  userController.getUserList
+    "/list",
+    validate(userValidation.getUserList),
+    userController.getUserList
 );
 
 /** Get user details by id */
 router.get(
-  "/get-details/:userId",
-  validate(userValidation.getDetails),
-  userController.getUserDetails
+    "/get-details/:userId",
+    validate(userValidation.getDetails),
+    userController.getUserDetails
 );
 
 /** user details update by id */
 router.put(
-  "/update-details/:userId",
-  validate(userValidation.updateDetails),
-  userController.updateDetails
+    "/update-details/:userId",
+    validate(userValidation.updateDetails),
+    userController.updateDetails
 );
 
 /** Delete user */
 router.delete(
-  "/delete-user/:userId",
-  validate(userValidation.getDetails),
-  userController.deleteUser
+    "/delete-user/:userId",
+    validate(userValidation.getDetails),
+    userController.deleteUser
 );
 
 /** Send mail */
 router.post(
-  "/send-mail",
-  validate(userValidation.sendMail),
-  userController.sendMail
+    "/send-mail",
+    validate(userValidation.sendMail),
+    userController.sendMail
 );
 
 module.exports = router;
