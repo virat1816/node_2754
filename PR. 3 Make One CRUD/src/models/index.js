dce9ba8dccb031d21dd1499687211426374ef6d5
virat1816 <116003414+virat1816@users.noreply.github.com>
2023-08-31 17:56:59 +0530

    Delete PR. 3 Make One CRUD directory

--- PR. 3 Make One CRUD/src/models/index.js
+++
@@ -1,7 +0,0 @@
-// module.exports.User = require("./user.model");
-// module.exports.Token = require("./token.model");
-
-module.exports.User = require("./user.model");
-module.exports.Category = require("./category.model");
-module.exports.Book = require("./book.model");
-module.exports.mobile = require("./mobile.model");
--- PR. 3 Make One CRUD/src/validations/index.js
+++
@@ -1,6 +0,0 @@
-// module.exports.userValidation = require("./user.validation");
-module.exports.categoryValidation = require("./category.validation");
-module.exports.userValidation = require("./user.validation");
-module.exports.bookValidation = require("./book.validation");
-module.exports.mobileValidation = require("./mobile.validation");
-
